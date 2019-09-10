f2627aee654447745aee8d401a0e8a9c00c6a042
sharoncr <sharon.raphael@batonsystems.com>
2019-09-10 19:19:37 +0530

    custom-property-files

--- src/main/java/com/mycom/thirdapp/MainClass.java
+++ src/main/java/com/mycom/thirdapp/MainClass.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @SpringBootApplication
+@PropertySources({
+    @PropertySource("classpath:${env}.properties"),
+    @PropertySource("classpath:base.properties")
+})
 public class MainClass extends SpringBootServletInitializer {
 
     public static void main(String[] args){
+        System.out.println(System.getenv("env"));
         SpringApplication.run(MainClass.class,args);
     }
 }
